47bc18d1c491509a3db7a1a6ab90a7ef67bffd6b
Gildson <37073818+Gildson@users.noreply.github.com>
2022-07-05 23:56:09 -0300

    Add files via upload

--- planet.c
+++ planet.c
@@ -9,7 +9,7 @@ Urano: 0,71 dias(day) de rotação e 84*365 dia(year) de translação
 Netuno: 0,67 dias(day) de rotação e 164*365 dia(year) de translação
 ------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-/*Planetas(raio)------------------------------------------------------------------------------------------------------------------
+/*Planetas(raio)----------------------------------------------------------------------------------------------------------------------------------
 Mercúrio: 2.439,7 km (0,11)
 Vênus: 6.051,8 km (0,28)
 Terra: 6.371 km (0.4)
@@ -28,6 +28,8 @@ Netuno: 24.622 km (1,16)
 #include <stdlib.h>
 #include <stdio.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "codes/stb_image.h"
 
 typedef struct{
 	float X;
@@ -62,7 +64,7 @@ int yInicial;
 int xMove = 0;
 int yMove = 0;
 float anguloAtualX = M_PI_2;
-float anguloAtualY = M_PI/6;
+float anguloAtualY = M_PI;
 int distCamera = 10;
 float focoCamX = 0;
 float focoCamY = 0;
@@ -70,7 +72,79 @@ float focoCamZ = 0;
 float offsetX = 0;
 float offsetZ = 0;
 
-void pCam(){
+GLuint texturaId[10];
+
+void carregaTextura(GLuint texId, char* filePath){
+    // Variável para guardar os dados da imagem
+    unsigned char* imgData;
+
+    // Variáveis para guardar os parâmetros da imagem
+    int largura, altura, canais;
+    
+    // Define um flip vertical de leitura da imagem
+	/**
+	 * x - onde começa a leitura
+	 * 
+	 *  ________________				 ________________
+	 * |x				        |				|				         |
+	 * |				        |				|				         |
+	 * |				        |				|				         |
+	 * |	imagem		    | 			|	  imagem	     |
+	 * |				        |		->	|				         |
+	 * |				        |				|				         |
+	 * |________________|				|x_______________|
+	 *    antes do flip						apos o flip
+	 * 
+	 * A mudança foi necessária devido ao opengl que tem como padrão de leitura
+	 * o canto inferior esquerdo
+	 */
+	stbi_set_flip_vertically_on_load(true);
+
+    // Leitura da imagem -> caminho/largura/altura/quantidade de canais no arquivo/quantidade de canais
+    imgData = stbi_load(filePath, &largura, &altura, &canais, 4);
+
+    if(imgData){
+        // Liga um espaço de memória no opengl ao texId passado para criar a textura
+        glBindTexture(GL_TEXTURE_2D, texId);
+    
+    // Criação da textura a partir de uma imagem 2D
+		// Parâmetros:
+		// 1 - O objeto: GL_TEXTURE_2D
+		// 2 - O nivel de detalhamento: para imagens é "0" 
+		// 3 - Especifica o formato das cores na textura: GL_RGBA8 (Red - Gree - Blue - Alpha de 8bits)
+		// 4 - lagura da textura: no momento da leitura da imagem foi atribuída a variável "largura"
+		// 5 - altura da textura: no momento da leitura da imagem foi atribuída a variável "altura"
+		// 6 - tamanho da borda: "0" (sem borda)
+		// 7 - Formato de cores dos dados da imagem: GL_RGBA
+		// 8 - Especifica o tipo de dado da imagem: GL_UNSIGNED_BYTE (8 bits sem sinal)
+		// 9 - Dados da imagem: Armazenado em "imgData"
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, largura, altura, 0, GL_RGBA, GL_UNSIGNED_BYTE, imgData);
+
+		// Resolução de problema de miniaturização (Textura com alta resolução quando distante da câmera)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		// Quando há necessidade de ampliação da textura
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// Repete a imagem em x (S no opengl) sempre que a largura da imagem for menor que a lagura da textura
+		// Para esse projeto a "lagura da Imagem" = "lagura da textura"
+		// devido ao parâmetro "largura" da textura ser o mesmo extraído da imagem
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+
+		// Repete a imagem em y (T no opengl) sempre que a altura da imagem for menor que a altura da textura
+		// Para esse projeto a "altura da Imagem" = "altura da textura"
+		// devido ao parâmetro "altura" da textura ser o mesmo extraído da imagem
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		// Liberação da memória ocupada pela imagem
+        stbi_image_free(imgData);
+    }
+    else {
+        printf("ERRO - Não foi possível carregar a Imagem! - ");
+    }
+}
+
+void posicao_cam(){
 	/**
 	 * Equação paramétrica da esfera no espaço (definição para esse projeto com up da câmera em y)
 	 * x = (raio)*cos(theta)*cos(phi)
@@ -121,8 +195,8 @@ void moveMouse(int x, int y){
 		yInicial = y;
 
 		// Limita a aproximação
-		if(distCamera < 1){
-			distCamera = 1;
+		if(distCamera < 2){
+			distCamera = 2;
 		}
 
 		// Limita o afastamento 
@@ -201,7 +275,26 @@ void init(void)
 
 	  // Habilita o depth-buffering
    glEnable(GL_DEPTH_TEST);
+   
+      glGenTextures(10, texturaId);
+
+    // Parâmetros da textura no objeto -> GL_REPLACE - Substitui a cor pela textura
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+   
+   	carregaTextura(texturaId[0], "texturas/Sol.jpg");
+	carregaTextura(texturaId[1], "texturas/Mercurio.jpg");
+	carregaTextura(texturaId[2], "texturas/Venus.jpg");
+	carregaTextura(texturaId[3], "texturas/Terra.jpg");
+	carregaTextura(texturaId[4], "texturas/Marte.jpg");
+	carregaTextura(texturaId[5], "texturas/Jupiter.jpg");
+	carregaTextura(texturaId[6], "texturas/Saturno.jpg");
+	carregaTextura(texturaId[7], "texturas/AnelSaturno.png");
+	carregaTextura(texturaId[8], "texturas/Urano.jpg");
+	carregaTextura(texturaId[9], "texturas/Netuno.jpg");
+	
+	glShadeModel (GL_SMOOTH);
 }
+
 
 void DefineIluminacao (void)
 {
@@ -240,86 +333,239 @@ void DefineIluminacao (void)
 
 void display(void)
 {
+
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    glLoadIdentity();
    glColor3f (1.0, 1.0, 1.0);
-   
-   pCam();
+   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_ADD);
 
-   glPushMatrix();
-   glutWireSphere(1, 20, 16);   /* draw sol */
+   posicao_cam();
    
+   glEnable(GL_TEXTURE_2D);
+      glPushMatrix();
+   glBindTexture(GL_TEXTURE_2D, texturaId[0]);
+   GLUquadricObj* sol = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( sol, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( sol, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( sol, GL_TRUE );
+
+   gluQuadricOrientation (sol, GLU_OUTSIDE);
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+   gluSphere ( sol, 1, 20, 16);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( sol );
+
+   //terra
+      
    glRotatef ((GLfloat) translacao_t, 0.0, 1.0, 0.0);
    glTranslatef (2.0, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_t, 0.0, 1.0, 0.0);
-   glutWireSphere(0.020, 10, 8);    /* draw terra*/
+   
+   glPushMatrix();
+   glBindTexture(GL_TEXTURE_2D, texturaId[3]);
+   GLUquadricObj* terra = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( terra, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( terra, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( terra, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+   gluQuadricOrientation (terra, GLU_OUTSIDE);
+   gluSphere ( terra, 0.020, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( terra );
 
    glPopMatrix();
-   
+  
+   //Mercurio
    glPushMatrix();
    glRotatef ((GLfloat) translacao_me, 0.0, 1.0, 0.0);
    glTranslatef(3.0, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_me, 0.0, 1.0, 0.0);
-   glutWireSphere(0.031, 10, 8);   /*draw mercurio*/
-   
+
+   glBindTexture(GL_TEXTURE_2D, texturaId[1]);
+   GLUquadricObj* me = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( me, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( me, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( me, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+   gluQuadricOrientation (me, GLU_OUTSIDE);
+   gluSphere ( me, 0.031, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( me );
+
    glPopMatrix();
 
+   //venus
    glPushMatrix();
    
    glRotatef ((GLfloat) translacao_v, 0.0, 1.0, 0.0);
    glTranslatef(4.0, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_v, 0.0, 1.0, 0.0);
-   glutWireSphere(0.050, 10, 8);   /*draw vernus*/
-   
+
+   glBindTexture(GL_TEXTURE_2D, texturaId[2]);
+   GLUquadricObj* v = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( v, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( v, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( v, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+      gluQuadricOrientation (v, GLU_OUTSIDE);
+   gluSphere ( v, 0.05, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( v ); 
+     
    glPopMatrix();
    
+   //marte
    glPushMatrix();
    
    glRotatef ((GLfloat) translacao_ma, 0.0, 1.0, 0.0);
    glTranslatef(5.0, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_ma, 0.0, 1.0, 0.0);
-   glutWireSphere(0.080, 10, 8);   /*draw marta*/
    
+   glBindTexture(GL_TEXTURE_2D, texturaId[4]);
+   GLUquadricObj* ma = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( ma, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( ma, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( ma, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+      gluQuadricOrientation (ma, GLU_OUTSIDE);
+   gluSphere ( ma, 0.08, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( ma );
    glPopMatrix();
    
+   //jupiter
    glPushMatrix();
    
    glRotatef ((GLfloat) translacao_j, 0.0, 1.0, 0.0);
    glTranslatef(6.0, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_j, 0.0, 1.0, 0.0);
-   glutWireSphere(0.329, 10, 8);   /*draw jupiter*/
+  
+   glBindTexture(GL_TEXTURE_2D, texturaId[5]);
+   GLUquadricObj* j = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( j, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( j, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( j, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+      gluQuadricOrientation (j, GLU_OUTSIDE);
+   gluSphere ( j, 0.329, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( j );
    
    glPopMatrix();
 
+  //saturno
    glPushMatrix();
    
    glRotatef ((GLfloat) translacao_s, 0.0, 1.0, 0.0);
    glTranslatef(7.00, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_s, 0.0, 1.0, 0.0);
-   glutWireSphere(0.274, 10, 8);   /*draw saturno*/
-   
+   glBindTexture(GL_TEXTURE_2D, texturaId[6]);
+   GLUquadricObj* s = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( s, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( s, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( s, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+      gluQuadricOrientation (s, GLU_OUTSIDE);
+   gluSphere ( s, 0.274, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( s );
    glPopMatrix();
 
+   //urano
    glPushMatrix();
    
    glRotatef ((GLfloat) translacao_u, 0.0, 1.0, 0.0);
-   glTranslatef(8.0, 0.0, 0.0);
+   glTranslatef(7.5, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_u, 0.0, 1.0, 0.0);
-   glutWireSphere(0.121, 10, 8);   /*draw urano*/
-   
+   glBindTexture(GL_TEXTURE_2D, texturaId[8]);
+   GLUquadricObj* u = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( u, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( u, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( u, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+      gluQuadricOrientation (u, GLU_OUTSIDE);
+   gluSphere ( u, 0.121, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( u );
    glPopMatrix();
 
+   //netuno
    glPushMatrix();
    
    glRotatef ((GLfloat) translacao_n, 0.0, 1.0, 0.0);
-   glTranslatef(9.0, 0.0, 0.0);
+   glTranslatef(8.0, 0.0, 0.0);
    glRotatef ((GLfloat) rotacao_n, 0.0, 1.0, 0.0);
-   glutWireSphere(0.116, 10, 8);   /*draw netuno*/
-   
+   glBindTexture(GL_TEXTURE_2D, texturaId[9]);
+   GLUquadricObj* n = gluNewQuadric ( );
+			      // Define o estilo de desenho  - GLU_FILL -> renderização das quádricas como polígonos primitivos
+   gluQuadricDrawStyle ( n, GLU_FILL );
+
+			      // Define os vetores normas da Quadric - GLU_SMOOTH -> Uma normal para cada vértice
+   gluQuadricNormals ( n, GLU_SMOOTH );
+			      // Habilita textura na Quadric
+   gluQuadricTexture ( n, GL_TRUE );
+
+			      // Desenha esfera com a Quadric - (lHori e lVert - corresponde a quantidade de meridianos e paralelos)
+      gluQuadricOrientation (n, GLU_OUTSIDE);
+   gluSphere ( n, 0.116, 10, 8);
+
+			      // Exclui a quadric
+   gluDeleteQuadric ( n );
    glPopMatrix();
 
+  
    DefineIluminacao();
-   
+
+   glDisable(GL_TEXTURE_2D);
+      
    glutSwapBuffers();
 }
 
@@ -354,6 +600,7 @@ void keyboard (unsigned char key, int x, int y)
          rotacao_n = (rotacao_n + 0,67) % 360;
          glutPostRedisplay();
          break;
+         
       case 'D':
          distCamera = 50;
 	 focoCamX = 0.0;
@@ -371,13 +618,9 @@ void keyboard (unsigned char key, int x, int y)
          rotacao_n = (rotacao_n - 0,67) % 360; 
          glutPostRedisplay();
          break;
+         
       case 'y':
          distCamera = 10;
-	 focoCamX = 0.0;
-	 focoCamY = 0.0;
-	 focoCamZ = 0.0;
-	 offsetX = 0.0;
-	 offsetZ = 0.0;
 
          translacao_t = (translacao_t + 365) % 360;
          translacao_me = (translacao_me + 87) % 360;
@@ -389,6 +632,7 @@ void keyboard (unsigned char key, int x, int y)
          translacao_n = (translacao_n + 59860) % 360;
          glutPostRedisplay();
          break;
+         
       case 'Y':
          distCamera = 50;
 	 focoCamX = 0.0;
@@ -412,12 +656,13 @@ void keyboard (unsigned char key, int x, int y)
    }
 }
 
+	
 int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
    glutInitWindowSize (1000, 1000); 
-   glutInitWindowPosition (100, 100);
+   glutInitWindowPosition (0, 0);
    glutCreateWindow (argv[0]);
    init ();
    glutDisplayFunc(display); 
